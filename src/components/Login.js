d6fc77b2623f27ae664f2df36724654d3fa018a5
andresf.murciaz <andresf.murciaz@gmail.com>
2022-12-06 17:43:42 -0500

    agrego compnente de login con su vista

---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+export function Login(){
+return<>
+
+
+
+<body class="container-fluid">
+<div class="col-md-4 offset-4">
+<main class="form-signin w-100 m-auto">
+  <form >
+    <img class="mb-4" src="../../public/logo192.png" alt="" width="72" height="57"/>
+    <h1 class="h3 mb-3 fw-normal col-md-4 offset-5" >Login </h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+      <label for="floatingInput">Usuario</label>
+    </div>
+
+    <div class="checkbox mb-2">
+      
+      </div>
+
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+      <label for="floatingPassword">Contraseña</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+   
+  </form>
+</main>
+</div>
+</body>
+</>
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ListaPeliculas } from './components/ListaPeliculas'; 
 import { Header,Menu } from './components/Header';
+import { Login } from './components/Login'
 import './css/bootstrap.min.css';
 import './css/bootstrap.min.css.map';
-
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +14,7 @@ root.render(<>
 <Menu/>
 <Header/>
 <ListaPeliculas/>
-
+<Login/>
 </>
 );
 
