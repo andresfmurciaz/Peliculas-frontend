9a43124a2353740916b200be8bffa6e1809535ba
andresf.murciaz <andresf.murciaz@gmail.com>
2022-12-07 11:52:47 -0500

    conexion con fech y parametros entre lista pelicula y pelicula

--- src/components/ListaPeliculas.js
+++ src/components/ListaPeliculas.js
@@ -1,27 +1,52 @@
 
 import {Pelicula} from './Pelicula'
+import {useState,useEffect} from 'react'
+
 
 export function ListaPeliculas(){
 
-fetch("http://localhost:3000/api/items")
-.then(response=>response.json())
-.then(data=>{console.log(data)})
-
-.catch(err =>  console.log("error" + err))
 
 
+
+
+    const [items,setItems]= useState([])
+
+
+    function cargar(){
+    fetch("http://localhost:3000/api/items")
+    .then(response=>response.json())
+    .then(data=>{console.log(data)
+      setItems(data)
+    })
+    .catch(err =>  console.log("error" + err))
+  }
+
+//para cargar lo de la clase
+  useEffect(()=>{cargar()},[])
+    
+  
 
     return<>
 <div className ="row my-2">
   <div className="container">
     <div className="row align-center">
       <div className = "col-m-5">
-        <Pelicula titulo="Tiburon 3"
-        imagen="https://www.rdstation.com/blog/wp-content/uploads/sites/2/2017/09/thestocks.jpg"
-         duracion = {4} 
-         tipo="Pelicula" 
-         anio="1937"
-          />
+        
+      {items.map(dato=>(
+
+
+         <Pelicula titulo ={dato.titulo}
+                   tipo ={dato.tipo} 
+                   ano={dato.ano}
+                   duracion={dato.duracion}
+                   img = {dato.img}
+                   genero={dato.genero}
+                   
+                   />
+      ))}
+          
+
+
 
         </div>
       </div>
--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -3,14 +3,14 @@ export  function Pelicula(props){
 return<>
 <div className = "col-3">
 <div class="card" >
-  <img src={props.imagen} class="card-img-top" alt="..."/>
+  <img src={props.img} class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">{props.titulo}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">{props.genero}</p>
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">{props.tipo}</li>
-    <li class="list-group-item">{props.anio}</li>
+    <li class="list-group-item">{props.ano}</li>
     <li class="list-group-item">{props.duracion}</li>
   </ul>
   <div class="card-body">
