3f513e8ceba0dbe1a32b2fed6cad2738b6083f55
andresf.murciaz <andresf.murciaz@gmail.com>
2022-12-14 18:59:35 -0500

    se elimino el token cuando se inicia seccion para que sea seguro el login

--- src/components/Dashboard/Tablero.js
+++ src/components/Dashboard/Tablero.js
@@ -1,4 +1,43 @@
-export function Tablero(){
 
-    return<>  <h1>hola tablero </h1></>
+
+import {useEffect} from 'react' 
+
+import {useNavigate} from 'react-router-dom'
+export function Tablero(){
+//guardo el token que ya esta generado en el login
+ //   const token = localStorage.getItem("tokrn")
+
+ const navigate =useNavigate()
+useEffect (()=>{
+console.log('validando inicio')
+try {
+const token= localStorage.getItem("token")    
+console.log(token)
+if(!token){
+navigate('/')
+}
+
+} catch (error) {
+    console.log('error')
+
+    navigate ('/')
+}
+
+
+})
+
+//funcion para eliminar el localstora y que se borre el token de ACCESO
+const hadleCerrar =()=>{
+localStorage.removeItem("token")
+navigate  ('/login')
+
+}
+
+    return<>  <h1>hola tablero </h1>
+    <button onClick={hadleCerrar}>cerrar</button>
+
+    </>
+
+
+
 }
